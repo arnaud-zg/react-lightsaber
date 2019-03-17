6683b9f09bcc4af882ee3c6c63eae1325e1734e6
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-03-17 14:46:50 +0100

    chore(test): get lightsaber owner from another file

--- __tests__/lightsaber.test.tsx
+++ __tests__/lightsaber.test.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactRenderer from 'react-test-renderer'
-import Lightsaber, { ELightsaberOwners } from '../src/Lightsaber'
+import Lightsaber from '../src/Lightsaber'
+import { ELightsaberOwners } from '../src/Lightsaber/owner'
 
 describe('Component.Lightsaber()', () => {
   it('should render component', () => {
     const component = ReactRenderer.create(
-      <Lightsaber id="1" owner={ELightsaberOwners.OBI} />,
+      <Lightsaber id="1" owner={ELightsaberOwners.OBI_WAN} />,
     )
 
     let tree = component.toJSON()
