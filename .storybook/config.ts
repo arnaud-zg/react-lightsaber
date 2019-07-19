ba4ce457b7d004895e4d869c934d81f6ae31bdcc
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-07-19 22:48:40 +0200

    chore(deps): remove dependency chromatic

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,5 +1,4 @@
 import { configure } from '@storybook/react'
-import 'storybook-chromatic'
 
 // automatically import all files ending in *.stories.tsx
 const req = require.context('../stories', true, /.stories.tsx$/)
